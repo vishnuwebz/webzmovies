2fef5e66911ea5fc41b9b897d83978958e859c05
Vishnu G <vishnugkannath@gmail.com>
2025-09-04 16:08:27 +0530

    fix: add STATIC_ROOT for collectstatic on Render

--- webzmovies/settings.py
+++ webzmovies/settings.py
@@ -153,10 +153,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# WhiteNoise (add right after SecurityMiddleware in MIDDLEWARE)
-# 'whitenoise.middleware.WhiteNoiseMiddleware'
-# and storage:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# WhiteNoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
